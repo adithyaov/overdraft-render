18f5f3c642c0490e7974efe1ff014604b0021eae
Adithya Kumar <functional.binder@gmail.com>
2026-03-13 01:13:30 +0530

    Update the tests

--- test/test_overdraft.ml
+++ test/test_overdraft.ml
@@ -7,14 +7,21 @@ let sample = {|
 #+NAME: principal
 |       Date | Amount    | Rate | Comments |
 |------------+-----------+------+----------|
-| 2026-03-04 | 35,00,000 |   10 |          |
-| 2026-03-04 | 32,50,000 |   12 |          |
-| 2026-03-05 | 5,00,000  |   12 |          |
+| 2025-03-04 | 35,00,000 |   10 |          |
+| 2025-06-04 | 35,00,000 |   10 |          |
+| 2025-09-04 | 32,50,000 |   12 |          |
+| 2026-01-05 | 5,00,000  |   12 |          |
 
 #+NAME: interest
-|       Date | Amount | Comments |
-|------------+--------+----------|
-| 2026-03-05 | 7,500  |          |
+|       Date | Amount   | Comments |
+|------------+----------+----------|
+| 2025-03-05 | 29,166   |          |
+| 2025-04-05 | 29,166   |          |
+| 2025-05-05 | 29,166   |          |
+| 2025-06-05 | 58,333   |          |
+| 2025-07-05 | 18,333   |          |
+| 2025-12-05 | 5,77,513 |          |
+| 2026-01-05 | 95,833   |          |
 
 * Bob
 
@@ -32,23 +39,28 @@ let sample = {|
 
 let expected =
   [("Alice",
-    {
-      unique_rates = [ 10; 12 ];
+    { unique_rates = [10; 12];
       principal_payments =
         [{ rate = 10; principal = 3500000;
-           date = { year = 2026; month = 3 } };
+           date = { year = 2025; month = 3 } };
+         { rate = 10; principal = 3500000;
+           date = { year = 2025; month = 6 } };
          { rate = 12; principal = 3250000;
-           date = { year = 2026; month = 3 } };
+           date = { year = 2025; month = 9 } };
          { rate = 12; principal = 500000;
-           date = { year = 2026; month = 3 } }
+           date = { year = 2026; month = 1 } }
         ];
       interest_payments =
-        [{ amount = 7500; date = { year = 2026; month = 3 } }]
-    }
-   );
+        [{ amount = 29166; date = { year = 2025; month = 3 } };
+         { amount = 29166; date = { year = 2025; month = 4 } };
+         { amount = 29166; date = { year = 2025; month = 5 } };
+         { amount = 58333; date = { year = 2025; month = 6 } };
+         { amount = 18333; date = { year = 2025; month = 7 } };
+         { amount = 577513; date = { year = 2025; month = 12 } };
+         { amount = 95833; date = { year = 2026; month = 1 } }]
+   });
    ("Bob",
-    {
-      unique_rates = [ 10 ];
+    { unique_rates = [10];
       principal_payments =
         [{ rate = 10; principal = 500000;
            date = { year = 2026; month = 3 } };
@@ -56,9 +68,7 @@ let expected =
            date = { year = 2026; month = 3 } }
         ];
       interest_payments =
-        [{ amount = 1500; date = { year = 2026; month = 3 } }]
-    }
-   )
+        [{ amount = 1500; date = { year = 2026; month = 3 } }] })
   ]
 
 let () =
