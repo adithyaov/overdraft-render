4dcd6ec1db7ae341930acbc86bdc2681bd5a6f3d
Adithya Kumar <functional.binder@gmail.com>
2026-03-15 19:07:45 +0530

    Wire the lib to the executable

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+open Overdraft_render
+
+let filepath = ref ""
+
+let usage_msg = "overdraft-render <path/to/your/file>.org"
+
+let speclist = []
+
+let () =
+  Arg.parse speclist (fun s -> filepath := s) usage_msg;
+  if !filepath = "" then (
+    prerr_endline "Error: No input file provided.";
+    Arg.usage speclist usage_msg;
+    exit 1
+  ) else (
+    !filepath
+    |> Parser.parse_file
+    |> Render.render_database
+  )
